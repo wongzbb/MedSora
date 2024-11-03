3619f2c435507d2b50539a30341428e25bd4e854
ZhenbinWang <2675589646@qq.com>
2024-11-04 00:17:15 +0800

    upload medsora train

---
+++ scripts/train_medsora_cho.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+cpuNum=4
+cudaID=3
+port=12749
+nnodes=1
+nproc_per_node=1
+config=./configs/cho/cho_train.yaml
+ptxasPath=/opt/conda/envs/test_sora/bin/ptxas  # run `which ptxas` to get the path
+
+model=MedSora-B
+epoch=300
+batchSize=2
+
+
+OMP_NUM_THREADS=$cpuNum \
+CUDA_VISIBLE_DEVICES=$cudaID \
+TRITON_PTXAS_PATH=$ptxasPath \
+torchrun \
+    --master_port=$port \
+    --nnodes=$nnodes \
+    --nproc_per_node=$nproc_per_node \
+    train.py \
+    --model $model \
+    --epoch $epoch \
+    --global-batch-size $batchSize \
+    --config $config \
+    --use-local-cov
